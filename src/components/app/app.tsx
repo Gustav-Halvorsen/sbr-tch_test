39b93bca8988b8cd87a93256d69c6f5accf70aa1
dmitry gorban <playmadperson@gmail.com>
2021-10-12 00:00:56 +0300

    added README.md

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { MainLayout } from "layouts";
 import { Routes } from "routes";
 import { useDispatch } from "react-redux";
-import { signIn } from "../../services/slices/user";
+import { signIn } from "services/slices/user";
 
 export const App: React.FC = () => {
   const dispatch = useDispatch();
